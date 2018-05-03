0ae30bd6295c1484eacead291f9bf22dc20dc441
David Schmotz <davidschmotz@gmx.de>
2018-05-03 22:04:10 +0200

    basic architecture build up

--- src/sidebar_src/actions.js
+++ src/sidebar_src/actions.js
@@ -4,32 +4,20 @@
 * @Email:  davidschmotz@gmail.com
 * @Filename: dragndrop.js
  * @Last modified by:   David
- * @Last modified time: 2018-05-03T16:03:02+02:00
+ * @Last modified time: 2018-05-03T21:25:20+02:00
 */
 
-const fs = require('fs');
+const ft = require("./filetree")
 
 var holder = document.getElementById('drag-file');
-
-holder.ondragover = () => {
-  return false;
-};
-
-holder.ondragleave = () => {
-  return false;
-};
-
-holder.ondragend = () => {
-  return false;
-};
-
+holder.ondragover = () => {return false};
+holder.ondragleave = () => {return false};
+holder.ondragend = () => {return false};
 holder.ondrop = (e) => {
   e.preventDefault();
-
   for (let f of e.dataTransfer.files) {
-
-    
+    let path = f.path
+    console.log(path);
+    ft.openPath(path);
   }
 }
-
-return false;
--- src/sidebar_src/filetree.js
+++ src/sidebar_src/filetree.js
@@ -1,23 +1,44 @@
 /**
- * @Author: David Schmotz <David>
- * @Date:   2018-05-03T15:04:36+02:00
- * @Email:  davidschmotz@gmail.com
- * @Filename: dragndrop.js
+* @Author: David Schmotz <David>
+* @Date:   2018-05-03T15:04:36+02:00
+* @Email:  davidschmotz@gmail.com
+* @Filename: dragndrop.js
  * @Last modified by:   David
- * @Last modified time: 2018-05-03T16:05:14+02:00
- */
+ * @Last modified time: 2018-05-03T22:03:17+02:00
+*/
+
+const fs = require('fs')
 
 function openPath(path) {
   document.getElementById("display-files").innerHTML = "";
-  console.log('File(s) you dragged here: ', f.path)
-  fs.readdir(f.path, (err, files) => {
+  console.log('File(s) you dragged here: ', path)
+  fs.readdir(path, (err, files) => {
     'use strict';
     //if an error is thrown when reading the directory, we throw it. Otherwise we continue
     if (err) throw  err;
     //the files parameter is an array of the files and folders in the path we passed. So we loop through the array, printing each file and folder
     for (let file of files) {
       console.log(file);
-      document.getElementById("display-files").innerHTML += `<li>${file}</li>`;
+      document.getElementById("display-files").innerHTML += `<a class="file">${file}</a>`;
     }
   });
+  addListenersForFiles()
 }
+
+function addListenersForFiles(classname = "file") {
+  const file_elements = document.getElementsByClassName(classname)
+  console.log(file_elements)
+  console.log(file_elements.length)
+  for (var i=0; i<file_elements.length; i++) {
+    console.log("lel");
+    // file_element.addEventListener("click", () => {
+    //   console.log("clicki");
+    // })
+  }
+}
+
+
+
+module.exports = {
+  openPath
+};
